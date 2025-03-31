9b52919248eb7180cd311569922bfe05cb05b19d
Tyrone Zheng <tyronez@umich.edu>
2025-03-31 10:01:06 -0400

    Attempt 1

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Recursive function to calculate the GCF
+int gcf(int A, int B) {
+    if (B == 0) {
+        return A;  // Base case: GCF(A, 0) = A
+    }
+    return gcf(B, A % B);  // Recursive case: GCF(A, B) = GCF(B, A % B)
+}
+
+int main() {
+    int A, B;
+
+    // Take input from the user
+    cout << "Enter two integer values you want to find the GCF of: ";
+    cin >> A >> B;
+
+    // Output the GCF
+    cout << "The greatest common factor of " << A << " and " << B << " is " << gcf(A, B) << endl;
+
+    return 0;
+}
 // Code to implement the gcf function goes here
